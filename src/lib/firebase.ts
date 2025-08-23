1329ea0d7919e3ecb7b0b30f0738390948b62ec6
Pablo R Holguin Araujo <Pholguin.a@gmail.com>
2025-08-23 19:43:33 +0000

    2025-08-23T19:18:14Z [web]  GET /?monospaceUid=311268 200 in 325ms 2025-

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,8 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// Using the default database to simplify debugging security rules.
-const db = getFirestore(app);
+// Using the 'alzadatos' database.
+const db = getFirestore(app, "alzadatos");
 const auth = getAuth(app);
 
 export { db, auth };
