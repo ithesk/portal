53c70fd46bd05fd07040d6600a0374d062d5a67e
Pablo R Holguin Araujo <Pholguin.a@gmail.com>
2025-08-23 20:09:20 +0000

    vamos a trabajar con el portal de autogestion cliente vamos a depuararlo

--- src/ai/dev.ts
+++
@@ -1,4 +0,0 @@
-// Flows will be imported for their side effects in this file.
-import './flows/link-equipment-flow';
-import './flows/fetch-requests-flow';
-import './flows/fetch-public-products-flow';
